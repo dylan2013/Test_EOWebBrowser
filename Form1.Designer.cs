b317b7695e694232e7d5829f05c82c6170ecae50
3DB2\dylan <dylan.huang@ischool.com.tw>
2021-12-27 17:16:27 +0800

    上傳一個版本

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Open_TestWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webControl1 = new EO.WinForm.WebControl();
+            this.webView1 = new EO.WebBrowser.WebView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Location = new System.Drawing.Point(12, 12);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(776, 351);
+            this.webControl1.TabIndex = 0;
+            this.webControl1.Text = "webControl1";
+            // 
+            // webView1
+            // 
+            this.webView1.InputMsgFilter = null;
+            this.webView1.ObjectForScripting = null;
+            this.webView1.Title = null;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "儲存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 415);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private EO.WinForm.WebControl webControl1;
+        private EO.WebBrowser.WebView webView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿
+using EO.WebBrowser.DOM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Open_TestWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            EO.WebBrowser.Runtime.AddLicense("mZvLn1mXwAAd457pzf8R7lnb5QUQvFuotcDcr2iptMPboVnq+fPw96ng9vYe" + "wK20psLcrmqns8PbsWqZpAcQ8azg8//ooWunprHavUaBpLHLn3Xq7fgZ4K3s" + "9vbpx23h9cXewI+/uNrsrnzm1QH++ajm+87ou2jq7fgZ4K3s9vbpjEOzs/0U" + "4p7l9/bpjEN14+30EO2s3MKetZ9Zl6TNF+ic3PIEEMidtbjD3rNtqrvJ47Vs" + "s7P9FOKe5ff29ON3hI6xy59Zs/D6DuSn6un26bto4+30EO2s3OnPuIlZl6Sx" + "5+Cl4/MI6YxDl6Sxy59Zl6TNDOOdl/gKG+R2mcng2cKh6fP+EKFZ7ekDHuio" + "5cGz3a9np6ax2r1GgaSxy591puX9F+6wtZE=");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileInfo projectDllURL = new FileInfo(Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", ""));
+            //取得html 相對路徑
+            string htmlRelativeURL = Path.Combine(projectDllURL.DirectoryName, "Redactor\\index.html");
+
+            webControl1.WebView = webView1;
+            webControl1.WebView.LoadUrl(htmlRelativeURL);
+
+        }
+
+        TaskScheduler ts { get; set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+        
+    }
+}
